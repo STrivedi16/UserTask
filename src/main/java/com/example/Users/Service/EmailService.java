e839c15f578db6db3f7186aeacf28ed52f234d25
STrivedi16 <shubhamtrivedi@nimapinfotech.com>
2023-02-23 21:46:18 +0530

    Swagger and Smtp

--- src/main/java/com/example/Users/Service/EmailService.java
+++ src/main/java/com/example/Users/Service/EmailService.java
@@ -59,7 +59,7 @@ public class EmailService {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				
 				
-				return  new  PasswordAuthentication("shubhamtrivedi@nimapinfotech.com", "shubhamT2711$");
+				return  new  PasswordAuthentication("username", "password");
 			}
 		
 			
